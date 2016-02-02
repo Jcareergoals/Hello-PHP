f58d70668750a5c6e7c5e2fe8c4471374f517b69
Jose Chery <Jcareergoals@hotmail.com>
2016-02-02 00:59:19 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>1st Azure Deployment</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Jose's first deployment with Azure">
+	<style type="text/css">
+		* {
+			margin: 0px; 
+			padding: 0px; 
+			font-family: tahoma;
+		}
+		body {	background-color: rgb(240,240,240); }
+		#container {
+			border: 2px solid black; 
+			width: 960px; 
+			margin: 40px auto; 
+			min-height: 250px; 
+			background-color: white; 
+			box-shadow: 0px 0px 10px inset;
+			padding: 10px; 
+		}
+		h1, p { text-align: center; }
+	</style>
+</head>
+<body>
+	<div id="container">
+		<h1><?php echo "Hello, MS Azure!"; ?></h1>
+		<p>This is Jose's first deployment using MS Azure. For more information on 
+		MS Azure, click <a href="https://azure.microsoft.com/en-us/?b=16.01">here</a>.</p>
+	</div> <!-- End Container -->
+</body> 
+</head>
